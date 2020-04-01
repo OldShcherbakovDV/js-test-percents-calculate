92c90f5f6cae7ec7ceeb5be6e07c5db0f499daf8
Данил Щербаков <randyydsh.work@gmail.com>
2020-04-01 23:35:02 +0300

    Added solution

--- convert-parts-to-percent.js
+++ convert-parts-to-percent.js
@@ -1,5 +1,38 @@
+const MAX_INPUT_LENGTH = 1000000;
+const COUNT_SYMBOLS_AFTER_POINT = 3;
+const PERCENT_COF = 100;
+const ZERO_PARTS_SUMM_RESULT = "100.000";
+
+const ERROR_MESSAGE_ARGUMENT_IS_NOT_ARRAY =
+  "Invalid input: argument is not array.";
+const ERROR_MESSAGE_EXCEEDED_MAX_LENGTH =
+  "Invalid input: exceeded array max length.";
+const ERROR_MESSAGE_UNEXPECTED_DATA_IN_ARRAY =
+  "Invalid input: unexpected data in array.";
+
 const convertPartsToPercent = parts => {
-  return [];
+  if (!Array.isArray(parts)) {
+    throw new Error(ERROR_MESSAGE_ARGUMENT_IS_NOT_ARRAY);
+  }
+  if (parts.length > MAX_INPUT_LENGTH) {
+    throw new Error(ERROR_MESSAGE_EXCEEDED_MAX_LENGTH);
+  }
+
+  const partsSumm = parts.reduce((summ, value) => {
+    if (typeof value === "string" || typeof value === "number") {
+      const number = Number(value);
+      if (number !== NaN && number >= 0) {
+        return summ + number;
+      }
+    }
+    throw new Error(ERROR_MESSAGE_UNEXPECTED_DATA_IN_ARRAY);
+  }, 0);
+
+  return partsSumm
+    ? parts.map(value =>
+        ((value * PERCENT_COF) / partsSumm).toFixed(COUNT_SYMBOLS_AFTER_POINT)
+      )
+    : [ZERO_PARTS_SUMM_RESULT];
 };
 
 module.exports = convertPartsToPercent;
--- tests/convert-parts-to-percent.test.js
+++ tests/convert-parts-to-percent.test.js
@@ -1,51 +1,55 @@
 const convertPartsToPercent = require("./../convert-parts-to-percent");
 
-const MAX_ARRAY_LENGTH = 1000000;
+const MAX_ARRAY_LENGTH = 1000001;
 
 describe("convertPartsToPercent", () => {
   describe("Errors", () => {
     it("Argument is not array", () =>
-      expect(convertPartsToPercent(true)).toThrowError(
+      expect(() => convertPartsToPercent(true)).toThrowError(
         new Error("Invalid input: argument is not array.")
       ));
     it("Argument is array but length more then max", () =>
-      expect(convertPartsToPercent(new Array(MAX_ARRAY_LENGTH))).toThrowError(
+      expect(() => convertPartsToPercent(new Array(MAX_ARRAY_LENGTH))).toThrowError(
         new Error("Invalid input: exceeded array max length.")
       ));
     it("Invaid values in array - object", () =>
-      expect(convertPartsToPercent(["1.5", 1.5, {}])).toThrowError(
+      expect(() => convertPartsToPercent(["1.5", 1.5, {}])).toThrowError(
         new Error("Invalid input: unexpected data in array.")
       ));
     it("Invaid values in array - object", () =>
-      expect(convertPartsToPercent(["1.5", 1.5, {}])).toThrowError(
+      expect(() => convertPartsToPercent(["1.5", 1.5, {}])).toThrowError(
         new Error("Invalid input: unexpected data in array.")
       ));
     it("Invaid values in array - boolean", () =>
-      expect(convertPartsToPercent(["1.5", 1.5, true])).toThrowError(
+      expect(() => convertPartsToPercent(["1.5", 1.5, true])).toThrowError(
         new Error("Invalid input: unexpected data in array.")
       ));
     it("Invaid values in array - Symbol", () =>
-      expect(convertPartsToPercent(["1.5", 1.5, Symbol()])).toThrowError(
+      expect(() => convertPartsToPercent(["1.5", 1.5, Symbol()])).toThrowError(
         new Error("Invalid input: unexpected data in array.")
       ));
     it("Invaid values in array - null", () =>
-      expect(convertPartsToPercent(["1.5", 1.5, null])).toThrowError(
+      expect(() => convertPartsToPercent(["1.5", 1.5, null])).toThrowError(
         new Error("Invalid input: unexpected data in array.")
       ));
     it("Invaid values in array - undefined", () =>
-      expect(convertPartsToPercent(["1.5", 1.5, undefined])).toThrowError(
+      expect(() => convertPartsToPercent(["1.5", 1.5, undefined])).toThrowError(
         new Error("Invalid input: unexpected data in array.")
       ));
     it("Invaid values in array - function", () =>
-      expect(convertPartsToPercent(["1.5", 1.5, () => 1])).toThrowError(
+      expect(() => convertPartsToPercent(["1.5", 1.5, () => 1])).toThrowError(
+        new Error("Invalid input: unexpected data in array.")
+      ));
+    it("Invaid values in array - string without number", () =>
+      expect(() => convertPartsToPercent(["1.5", 1.5, "67px"])).toThrowError(
         new Error("Invalid input: unexpected data in array.")
       ));
   });
   describe("Calculations", () => {
     it("Array length is 0", () =>
-      expect(convertPartsToPercent([])).toEqual([]));
+      expect(convertPartsToPercent([])).toEqual(["100.000"]));
     it("Array length is 1", () =>
-      expect(convertPartsToPercent([221])).toEqual([221]));
+      expect(convertPartsToPercent([221])).toEqual(["100.000"]));
     it("Persent of 0", () =>
       expect(convertPartsToPercent([0])).toEqual(["100.000"]));
     it("Array with even number of elements", () =>
